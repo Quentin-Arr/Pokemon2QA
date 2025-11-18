a220177bbd5558e2126d8bfc3cfd1188597ea87c
quent <quent@172.17.37.154>
2025-11-18 10:57:38 +0100

    main tests

--- src/main.java
+++ src/main.java
@@ -1,6 +1,31 @@
 
 public class main {
-	public static void main(String[] args) {
-		
+	/*private static void testPokemon0() {
+	    Pokemon p1 = new Pokemon("Rhinolove");
+	    Pokemon p2 = new Pokemon("Chovsourir");
+	    p1.log(p1.toString());
+	    p2.log(p2.toString());
+	    p1.attaquer(p2);
+	    p2.log(p2.toString());
+	}*/
+
+	private static void testPokemonTypes() {
+	    PokemonEau eau = new PokemonEau("PokéEau");
+	    PokemonFeu feu = new PokemonFeu("PokéFeu");
+	    eau.log(eau.toString());
+	    feu.log(feu.toString());
+	    eau.attaquer(feu);
+	    feu.attaquer(eau);
+	    eau.log(eau.toString());
+	    feu.log(feu.toString());
 	}
+
+	public static void main(String[] args) {
+	    //System.out.println("\nTEST POKEMON0 \n");
+	    //testPokemon0();
+
+	    System.out.println("\nTEST POKEMON TYPES \n");
+	    testPokemonTypes();
+	}
+
 }
